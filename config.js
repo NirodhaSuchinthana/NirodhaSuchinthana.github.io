75beaeba8951900af612bdfdbff82e48f0b2a585
Nirodha Suchinthana <suchithanaapn@gmail.com>
2020-07-22 02:02:52 +0530

    gestby sample template

---
+++ config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  siteTitle: 'NirodhaS', // <title>
+  manifestName: 'Identity',
+  manifestShortName: 'Landing', // max 12 characters
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#663399',
+  manifestThemeColor: '#663399',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/img/website-icon.png', // This path is subpath of your hosting https://domain/portfolio
+  authorName: 'Nirodha Suchinthana',
+  heading: 'Software Engineer',
+  // social
+  socialLinks: [
+    {
+      icon: 'fa-github',
+      name: 'Github',
+      url: 'https://github.com/anubhavsrivastava',
+    },
+    {
+      icon: 'fa-twitter',
+      name: 'Twitter',
+      url: 'https://twitter.com/onlyanubhav',
+    },
+    {
+      icon: 'fa-facebook',
+      name: 'Facebook',
+      url: 'https://facebook.com/theanubhav',
+    },
+    {
+      icon: 'fa-envelope-o',
+      name: 'Email',
+      url: 'mailto:anubhav.srivastava00@gmail.com',
+    },
+  ],
+};
